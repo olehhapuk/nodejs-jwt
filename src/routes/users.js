75f3fdb251e6b5511e710c8f6acb48a2da7113ca
oleggapuk <oleggapuk20@gmail.com>
2021-04-16 14:02:03 +0300

    Added POST /users/login

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const { User } = require('../schemas');
 const { validateBySchema } = require('../middlewares');
-const { registerUserSchema } = require('../validationSchemas');
+const { registerUserSchema, loginUserSchema } = require('../validationSchemas');
 const { jwt } = require('../utils');
 
 const router = express.Router();
@@ -57,7 +57,51 @@ router.post(
     }
   }
 );
-// router.post('/login', UserController.login);
+
+router.post('/login', validateBySchema(loginUserSchema), async (req, res) => {
+  try {
+    // Check if user with this email exists
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (!user) {
+      res.status(404).send('No user found with this email');
+      return;
+    }
+
+    // Compare passwords
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      res.status(401).send('Invalid credentials');
+      return;
+    }
+
+    // Generate jwt token
+    const payload = {
+      _id: user._id,
+    };
+
+    const token = await jwt.generate(payload);
+
+    res.json({
+      user: {
+        _id: user._id,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+      token,
+    });
+  } catch (error) {
+    console.dir(error, { depth: null });
+    res.status(500).json(error);
+  }
+});
 // router.get('/me', authorize, UserController.me);
 
 module.exports = router;
--- src/validationSchemas/index.js
+++ src/validationSchemas/index.js
@@ -1,3 +1,4 @@
 module.exports = {
   registerUserSchema: require('./registerUserSchema'),
+  loginUserSchema: require('./loginUserSchema'),
 };
